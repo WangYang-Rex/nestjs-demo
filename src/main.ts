358f6e234877372bdf568fc8e371fb46c2a4c95b
王洋 <dayang.wy@raycloud.com>
2024-07-04 17:05:54 +0800

    ✨ feat: guard

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { logger } from './middleware/logger.middleware';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ValidationPipe } from './pipe/validate.pipe';
+import { RolesGuard } from './gard/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局中间件
   app.use(logger);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new RolesGuard());
+  // app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
